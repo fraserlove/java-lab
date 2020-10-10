1fe714a4fd0ca4fa0f2591906f7d981abd1b2a89
Fraser Love <me@fraser.love>
2020-10-10 23:27:11 +0100

    Changed project name

--- W05 - JUnit/code/src/common/LaneCodeAlreadyInUseException.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-/**
- * This exception should be used to indicate the illegal state when multiple products share the same lane in the vending machine.
- *
- */
-public class LaneCodeAlreadyInUseException extends Exception {
-}
--- W05 - JUnit/code/src/common/LaneCodeNotRegisteredException.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-/**
- * This exception should be used to indicate that a lane code has been presented to the system before it has been registered for use for an item.
- *
- */
-public class LaneCodeNotRegisteredException extends Exception {
-}

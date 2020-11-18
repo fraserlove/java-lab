7a454ea813c411d08274ccb31b3ed01a66e5d4ed
Fraser Love <me@fraser.love>
2020-11-18 17:00:52 +0000

    completed W10 practical

--- W10 - Complexity/quicksort-complexity-analysis/graph_generator.py
+++ W10 - Complexity/quicksort-complexity-analysis/graph_generator.py
@@ -1,57 +1,52 @@
 import csv
+import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-import numpy as np
 
-sortedness = []
-list_size = []
-execution_time = []
+matplotlib.rcParams.update({'font.size': 14})
 
-# Must be same number of max swaps as in Quicksort
-MAX_SWAPS = 200
+edit_distances, array_sizes, execution_times = [], [], []
+
+# Must be same value as used within the Java QuickSortComplexityAnalyser class
+RESULTS_RESOLUTION = 50
 
 with open('results.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=' ')
     for row in csv_reader:
-        list_size.append(float(row[0]))
-        if (float(row[1])) == 0.0:
-            print("Yes")
-        sortedness.append(float(row[1]))
-        execution_time.append(float(row[2]))
+        array_sizes.append(float(row[0]))
+        edit_distances.append(float(row[1]))
+        execution_times.append(float(row[2]))
 
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.plot_trisurf(list_size, sortedness, execution_time, edgecolor='none', linewidth=0, cmap="viridis", zorder=1)
-
-# Obtaining lists of the values when the number of swaps is at a maximum and a minimum, this is so lines to show how the
+# Obtaining arrays of the values when the number of swaps is at a maximum and a minimum, this is so lines to show how the
 # quicksort algorithm scales up on larger arrays when the array is sorted or not can be drawn.
 x_max, y_max, z_max, x_min, y_min, z_min = [], [], [], [], [], []
-for i in range(len(list_size)):
-    if i % MAX_SWAPS == 0:
-        x_max.append(list_size[i])
-        y_max.append(sortedness[i])
-        z_max.append(execution_time[i])
-    if i % MAX_SWAPS == MAX_SWAPS - 1:
-        x_min.append(list_size[i])
-        y_min.append(sortedness[i])
-        z_min.append(execution_time[i])
+for i in range(len(array_sizes)):
+    if i % RESULTS_RESOLUTION == 0:
+        x_max.append(array_sizes[i])
+        y_max.append(edit_distances[i])
+        z_max.append(execution_times[i])
+    if i % RESULTS_RESOLUTION == RESULTS_RESOLUTION - 1:
+        x_min.append(array_sizes[i])
+        y_min.append(edit_distances[i])
+        z_min.append(execution_times[i])
 
-ax.plot(x_max, y_max, z_max, label="$O(n^2)$", color="red", zorder=3, linewidth=4)
-ax.plot(x_min, y_min, z_min, label="$O(nlog(n))$", color="green", zorder=3, linewidth=4)
-ax.legend()
-ax.set_xlabel('List Size')
+# Creating 3D graph
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_trisurf(array_sizes, edit_distances, execution_times, edgecolor='none', linewidth=0, cmap=cm.inferno, zorder=1, antialiased=False)
+ax.plot(x_max, y_max, z_max, label="$O(n^2)$", color="#f20089", zorder=3, linewidth=3)
+ax.plot(x_min, y_min, z_min, label="$O(nlog(n))$", color="#8900f2", zorder=3, linewidth=3)
+ax.set_xlabel('Array Size')
 ax.set_ylabel('Sortedness (edit distance)')
 ax.set_zlabel('Execution Time (ms)')
-plt.savefig('execution_time_vs_no_swaps_and_array_size_3d.png')
-plt.show()
+plt.legend()
 
-# Using largest array to draw 2d graph showing the relationship between sortedness and execution time.
-sortedness_max = sortedness[len(sortedness) - 100:]
-execution_time_max = execution_time[len(execution_time) - 100:]
-
-plt.plot(sortedness_max, execution_time_max, color="purple")
-plt.xlabel('Sortedness (no swaps)')
-plt.ylabel('Execution Time (ms)')
-plt.savefig('execution_time_vs_no_swaps.png')
-plt.show()
+# Adjusting positioning of graph for images
+fig.set_size_inches(12.5, 12)
+ax.view_init(elev=20, azim=-245)
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+plt.tight_layout()
+plt.savefig("sortedness_vs_time_vs_size.png", pad_inches=0)
+ax.view_init(elev=-3, azim=98)
+plt.savefig("sortedness_vs_time_vs_size_2.png", pad_inches=0)
--- W10 - Complexity/quicksort-complexity-analysis/src/QuickSortComplexityAnalyser.java
+++ W10 - Complexity/quicksort-complexity-analysis/src/QuickSortComplexityAnalyser.java
@@ -4,19 +4,23 @@ import java.util.*;
 
 public class QuickSortComplexityAnalyser {
 
-    private static final int WARMUP_ITERATIONS = 5;
+    private static final int WARMUP_ITERATIONS = 10;
     // The higher the number of iterations the more accurate the average quicksort execution time will be, as this will
     // gradually decrease the effect that significant long runs due to background processes have in the overall average
-    // calculated.
-    private static final int TIMED_ITERATIONS = 100; // 500
-    private static final int MIN_ARRAY_SIZE = 2;
-    private static final int MAX_ARRAY_SIZE = 1000; // 5000
-    // The max number of swaps the array should receive. Normally this number is the same as the array size but can
-    // differ, if the user wants to test how further swapping may effect performance (especially since swapping an array
-    // of size n, n times may result in some elements being swapped twice (with a small chance of 1/n^2), leading to some
-    // not being swapped at all, and so some factor of sortedness may remain).
-    private static final int MAX_SWAPS = 200;
-    private static final int MIN_SWAPS = 0;
+    // calculated, reducing the overall error.
+    private static final int TIMED_ITERATIONS = 500;
+    private static final int MIN_ARRAY_SIZE = 0;
+    private static final int MAX_ARRAY_SIZE = 5000;
+    // The max number of swaps the array should receive, this could be the same value as the array size, however this
+    // increases the number of swaps needed to be performed to each array size and so increases performance. The general
+    // relationship between the max swaps and execution time can be found for values of max swaps that are significantly
+    // smaller than the maximum array size.
+    private static final int MAX_EDIT_DISTANCE = 100;
+    private static final int MIN_EDIT_DISTANCE = 0;
+    // Used to make sure results are of a specific length, and allowing for lists to iterate through to the max edit
+    // distance and max array size with a step so that a certain number (resolution) of results is calculated for each.
+    private static final int RESULTS_RESOLUTION = 50;
+    private static final boolean ASC_SORT_ORDER = false;
 
     private static Random random = new Random();
 
@@ -38,16 +42,26 @@ public class QuickSortComplexityAnalyser {
         return array;
     }
 
-    /*
-    Swaps two random cards a total of n times to shuffle the array. An array with infinitely many shuffles can be
-    considered truly random. This shuffling allows for a sorted array to be changed into an array of varying sortedness,
-    since it can specify the number n, of swaps needed for it to be back in a sorted state.
-     */
-    private static int[] shuffle(int[] array, int n) {
+    private static int[] generateSequentialIntegers(int n) {
+        return generateSequentialIntegers(n, true);
+    }
 
-        int[] shuffled = Arrays.copyOf(array, array.length); // Creating copy of array, so that it isn't shuffled.
-        for (int i = 0; i < n; i++) {
-            swap(shuffled, random.nextInt(shuffled.length), random.nextInt(shuffled.length));
+    /*
+    Swaps two random elements a total of n times to shuffle the array and increase edit distance. This shuffling allows for
+    a sorted array to be changed into an array with a specified edit distance, since it can specify the minimum number
+    of swaps needed for it to be sorted back into a sorted state. There is a small chance that the same element will be
+    chosen twice randomly (1/n^2 chance) or that a swap will undo the swap previous to it and lead to the edit distance
+    not increasing at all (1/n^4 chance), however the probability of this happening is very small and decreases with
+    the size of the array. Therefore for suitably large arrays, this is not a significant issue.
+     */
+    private static int[] increaseEditDistance(int[] array, int editDistance) {
+
+        // Creating copy of the input array, so that it isn't shuffled.
+        int[] shuffled = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < editDistance; i++) {
+            if (array.length > 0) {
+                swap(shuffled, random.nextInt(shuffled.length), random.nextInt(shuffled.length));
+            }
         }
         return shuffled;
     }
@@ -121,57 +135,54 @@ public class QuickSortComplexityAnalyser {
     Finds the mean execution time in ms when sorting the provided array. The mean value is taken over a certain number of
     iterations. The method tries to remove as much error and produce as accurate results as possible. This is done using
     the following:
-      - warmup iterations to optimise JVM code and case usage before the real test is done.
+      - warm-up iterations to optimise JVM code and case usage before the real test is done.
       - many timed iterations to average out the results and decrease the factor that the smallest and largest times have
         on the final result.
-      - only timing the time that quicksort is being performed and nothing else, so the timing is as accurate as possible
-      - shuffling the array differently each iteration, but for the same number of swaps. This ensures that the timings
-        obtained are not from a rare bad case scenario where a terrible swap was performed or vice versa. These are
-        averaged out among all the possible swaps that could occur.
-      - discarding the lowest and highest 20th percentile results so that only the most accurate results are left, since
+      - only timing the time that quicksort is being performed and nothing else, so the timing is as accurate as possible.
+      - shuffling the array differently each iteration, but for the same number of swaps (edit distance). This ensures
+        that the timings obtained are not from a rare bad case scenario where a terrible swap was performed or vice versa.
+        These are averaged out among all the possible swaps that could occur.
+      - discarding the lowest and highest 10th percentile results so that only the most accurate results are left, since
         values significantly smaller or larger than the median are removed.
      */
-    private static double[] findMeanSortTime(int[] array, int swaps) {
+    private static double findMeanSortTime(int[] array, int swaps) {
 
         long startTime;
         List<Long> times = new ArrayList<>();
-        SpearmansCorrelationCoefficient spearmansCoefficient = new SpearmansCorrelationCoefficient();
-        double spearmans = 0;
 
         // Warm up iterations to allow for JVM to optimise code and cache usage, increasing timing accuracy.
         for (int i = 0; i < WARMUP_ITERATIONS; i++) {
-            quicksort(shuffle(array, swaps));
+            quicksort(increaseEditDistance(array, swaps));
         }
 
         for (int i = 0; i < TIMED_ITERATIONS; i++) {
             // Each iteration, shuffling the array by swapping a certain number of times.
-            int[] shuffled = shuffle(array, swaps);
+            int[] unsorted = increaseEditDistance(array, swaps);
 
             // Timing only the quicksort method to be as accurate as possible.
             startTime = System.nanoTime();
-            spearmans = spearmansCoefficient.spearman(array, shuffled);
-            quicksort(shuffled);
+            quicksort(unsorted);
             times.add(System.nanoTime() - startTime);
 
             // Checking each iteration if quicksort has worked correctly and produced a sorted array. This is done by
             // comparing the sorted array to the generated sequential array.
-            if (!Arrays.equals(shuffled, generateSequentialIntegers(array.length, true))) {
+            if (!Arrays.equals(unsorted, generateSequentialIntegers(array.length))) {
                 System.out.println("Error: quicksort did not correctly sort the array.");
             }
         }
 
+        // using java standard library, since will have a more optimised sorting algorithm.
         Arrays.sort(times.toArray());
-        int bounds = times.size() / 5;
+        int bounds = TIMED_ITERATIONS / 10;
         long sum = 0;
         for (int i = 0; i < times.size(); i++) {
-            // discarding the lowest 20% and highest 20% of results to obtain most accurate results.
-            if (i > bounds && i < times.size() - bounds) {
+            // discarding the lowest 10% and highest 10% of execution times to obtain most accurate results.
+            if (i >= bounds && i <= times.size() - bounds) {
                 sum += times.get(i);
             }
         }
-        // Return the new average from the median 60% of values left and convert from ns to ms
-        double[] result = {sum / 1e6 * (TIMED_ITERATIONS - (bounds * 2)), spearmans};
-        return result;
+        // Return the new average from the median 80% of values left and convert from ns to ms
+        return sum / (1e6 * (TIMED_ITERATIONS - (bounds * 2)));
     }
 
     /*
@@ -183,15 +194,13 @@ public class QuickSortComplexityAnalyser {
     private static void generateSortingResults() {
 
         List<Double[]> results = new ArrayList<>();
-        boolean ascending = true;
 
-        for (int arraySize = MIN_ARRAY_SIZE; arraySize < MAX_ARRAY_SIZE; arraySize += MAX_ARRAY_SIZE / 100) {
-            for (int swaps = MIN_SWAPS; swaps < MAX_SWAPS; swaps += MAX_SWAPS / 100) {
-                double[] x = findMeanSortTime(generateSequentialIntegers(arraySize, ascending), swaps);
-                Double[] result = {(double) arraySize, x[1], x[0]};
+        for (int arraySize = MIN_ARRAY_SIZE; arraySize < MAX_ARRAY_SIZE; arraySize += MAX_ARRAY_SIZE / RESULTS_RESOLUTION) {
+            for (int swaps = MIN_EDIT_DISTANCE; swaps < MAX_EDIT_DISTANCE; swaps += MAX_EDIT_DISTANCE / RESULTS_RESOLUTION) {
+                Double[] result = {(double) arraySize, (double) swaps, findMeanSortTime(generateSequentialIntegers(arraySize, ASC_SORT_ORDER), swaps)};
                 results.add(result);
             }
-            System.out.println("Completed array: " + arraySize + " out of " + MAX_ARRAY_SIZE);
+            System.out.println("Generating Results: (" + arraySize + "/" + MAX_ARRAY_SIZE + ")");
         }
         writeToCSV(results);
     }
